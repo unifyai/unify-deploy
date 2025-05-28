34346b75a2aa662a9f8f0ae25c1b211c381e8ac2
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-05-28 16:00:06 +0530

    updated topic for picking up emails and disabled the final notifications for now

--- adapters/email/main.py
+++ adapters/email/main.py
@@ -34,7 +34,7 @@ def renew_watch():
             # Configure watch request
             watch_request = {
                 "labelIds": ["INBOX"],
-                "topicName": f"projects/{os.getenv('PROJECT_ID')}/topics/email-notifications-test",
+                "topicName": f"projects/{os.getenv('PROJECT_ID')}/topics/email-notifications",
             }
 
             # Execute watch request
@@ -92,7 +92,8 @@ def process_notification(cloud_event):
             # ToDo: check if the conversation stored for this thread_id has changed
             # send the thread_id to a different channel
             print(f"Successfully processed conversation for user {user_id}")
-            publish_thread_id(thread_id, user_id)
+            # ToDo: disabled for now
+            # publish_thread_id(thread_id, user_id)
             return "OK"
         else:
             print(f"No new conversations found for user {user_id}")
