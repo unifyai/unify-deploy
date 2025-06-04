ea99f678b82e4365b8d74e98ed38262033e0383f
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-06-04 18:32:37 +0530

    added the USER_PHONE_NUMBER env var to the infra cloud run job

--- communication/infra/views.py
+++ communication/infra/views.py
@@ -169,6 +169,10 @@ async def create_cloudrun_job(
                                     value=user_number,
                                 ),
                                 run_v2.EnvVar(
+                                    name="USER_PHONE_NUMBER",
+                                    value=user_number,
+                                ),
+                                run_v2.EnvVar(
                                     name="UNITY_COMMS_URL",
                                     value_source=run_v2.EnvVarSource(
                                         secret_key_ref=run_v2.SecretKeySelector(
