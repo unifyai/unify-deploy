6c98e17741eebc354357e2498fd33ecb13fa1855
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-06-09 12:35:29 +0530

    minor change to admin key being passed to start unity service

--- adapters/phone/main.py
+++ adapters/phone/main.py
@@ -56,12 +56,10 @@ def start_service_if_not_running(assistant_id: str):
     Start the service if it is not running.
     """
     service_url = f"https://unity-{assistant_id}-000000000000.us-central1.run.app"
-    response = requests.get(f"{service_url}/status").json()
+    headers = {"Authorization": f"Bearer {os.getenv('ORCHESTRA_ADMIN_KEY')}"}
+    response = requests.get(f"{service_url}/status", headers=headers).json()
     if not response["running"]:
-        response = requests.post(
-            f"{service_url}/start",
-            headers={"Authorization": f"Bearer {os.getenv('ORCHESTRA_ADMIN_KEY')}"},
-        )
+        response = requests.post(f"{service_url}/start", headers=headers)
         if response.status_code != 200:
             print(f"Failed to start service for assistant {assistant_id}")
 
