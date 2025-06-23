99d5b40c8678feca0f589d384fae6a8c76fa3c80
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-06-23 14:21:44 +0530

    updated start service if not running to not break for assistant 2 for now

--- adapters/whatsapp/main.py
+++ adapters/whatsapp/main.py
@@ -49,7 +49,7 @@ def start_service_if_not_running(assistant_id: str):
     """
     Start the service if it is not running.
     """
-    if assistant_id not in ["default-assistant", "default-assistant-2", "default-assistant-3"]:
+    if assistant_id not in ["default-assistant"]:#, "default-assistant-2", "default-assistant-3"]:
         service_url = f"https://unity-{assistant_id}-000000000000.us-central1.run.app"
         headers = {"Authorization": f"Bearer {os.getenv('ORCHESTRA_ADMIN_KEY')}"}
         response = requests.get(f"{service_url}/status", headers=headers).json()
