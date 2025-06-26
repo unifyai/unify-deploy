311ac27b9d0954ae1cecf428b093013f36dec108
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-06-26 13:33:14 +0800

    switched the other way round to test if the response is better

--- adapters/email/helpers.py
+++ adapters/email/helpers.py
@@ -87,7 +87,7 @@ def get_thread_id(user_id, history_id, gmail_service):
             .list(
                 userId=user_id,
                 startHistoryId=history_id,
-                labelId="UNREAD",
+                historyTypes=["messageAdded", "labelAdded"],
             )
             .execute()
         )
@@ -99,7 +99,7 @@ def get_thread_id(user_id, history_id, gmail_service):
                 .list(
                     userId=user_id,
                     startHistoryId=history_id,
-                    historyTypes=["labelAdded"],
+                    labelId="UNREAD",
                 )
                 .execute()
             )
