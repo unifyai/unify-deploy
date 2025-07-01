9b1b8937b0e169b40de541fb24aae251f79f469b
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-07-01 15:14:30 +0800

    added reload to app in local dev mode

--- main.py
+++ main.py
@@ -23,4 +23,4 @@ async def read_root():
     return {"message": "success!"}
 
 if __name__ == "__main__":
-    uvicorn.run(app, host='0.0.0.0', port=8080)
+    uvicorn.run("main:app", host='0.0.0.0', port=8080, reload=True)
