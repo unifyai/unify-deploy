38fe56118d9554e1f1ac9d1efdbd12ec6a9b1f54
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-07-09 16:57:03 +0530

    increased resource slightly

--- communication/infra/helpers.py
+++ communication/infra/helpers.py
@@ -252,8 +252,8 @@ def create_unity_job(
                                     {"name": "MKL_NUM_THREADS", "value": "2"},
                                 ],
                                 "resources": {
-                                    "requests": {"cpu": "1", "memory": "4Gi"},
-                                    "limits": {"cpu": "1", "memory": "4Gi"},
+                                    "requests": {"cpu": "2", "memory": "8Gi"},
+                                    "limits": {"cpu": "2", "memory": "8Gi"},
                                 },
                                 "volumeMounts": [
                                     {
