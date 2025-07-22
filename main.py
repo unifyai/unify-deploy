5a521cf31b173eb42d70ed417db6034925d0148a
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-07-22 16:25:51 +0530

    minor change

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 from fastapi import FastAPI, Depends
-from communication.phone.views import router as phone_router
+from communication.phone.views import (
+    auth_router as phone_auth_router,
+    unauth_router as phone_unauth_router
+)
 from communication.whatsapp.views import router as whatsapp_router
 from communication.email.views import router as email_router
 from communication.infra.views import router as infra_router
@@ -12,7 +15,8 @@ load_dotenv(override=True)
 
 admin_auth = [Depends(auth_admin_key)]
 app = FastAPI()
-app.include_router(phone_router, prefix="/phone", dependencies=admin_auth)
+app.include_router(phone_auth_router, prefix="/phone", dependencies=admin_auth)
+app.include_router(phone_unauth_router, prefix="/phone", dependencies=[])
 app.include_router(whatsapp_router, prefix="/whatsapp", dependencies=admin_auth)
 app.include_router(email_router, prefix="/email", dependencies=admin_auth)
 app.include_router(infra_router, prefix="/infra", dependencies=admin_auth)
