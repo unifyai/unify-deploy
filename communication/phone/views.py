38171831535a92e25524c75498de3aa6cda88f7d
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-08-27 15:38:09 +0530

    added more status callback events for testing the call status endpoint for outbound calls

--- communication/phone/views.py
+++ communication/phone/views.py
@@ -84,7 +84,7 @@ def add_user_to_conference(
         from_=from_number,
         twiml=str(response),
         status_callback=f"{os.getenv('UNITY_COMMS_URL')}/phone/call-status",
-        status_callback_event=["initiated"],
+        status_callback_event=["initiated", "answered", "completed"],
     )
     return call.sid
 
