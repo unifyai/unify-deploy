bb7800558de76b5298f6e4e1fa0bec6878d94084
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-09-02 19:27:26 +0530

    added Number instead of just dial with the status callback instead of the action url for dial directly

--- communication/phone/views.py
+++ communication/phone/views.py
@@ -509,12 +509,11 @@ async def twiml(request: Request):
     phone_number = "+" + request.query_params.get("phone_number")
     print("TWIML phone number:", phone_number)
     resp_user = VoiceResponse()
-    resp_user.dial(
+    dial = resp_user.dial(caller_id=twilio_number, answer_on_bridge=True)
+    dial.number(
         phone_number,
-        action=f"{os.getenv('UNITY_COMMS_URL')}/phone/call-status",
-        caller_id=twilio_number,
-        answer_on_bridge=True,
-        events=["initiated", "ringing", "answered", "completed"],
+        status_callback_event="initiated ringing answered completed",
+        status_callback=f"{os.getenv('UNITY_COMMS_URL')}/phone/number-status",
     )
     print("TWIML response:", str(resp_user))
     return Response(status_code=200, content=str(resp_user), media_type="text/xml")
