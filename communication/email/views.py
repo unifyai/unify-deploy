60f98ff6a40fb4620d7a9827302505ae09e25496
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-10-01 15:42:17 +0800

    formatting fix

--- communication/email/views.py
+++ communication/email/views.py
@@ -171,7 +171,12 @@ async def watch_email(request: Request):
 
 
 @router.get("/attachment")
-async def get_attachment(sender_email: str, gmail_message_id: str, attachment_id: str, filename: str | None = None):
+async def get_attachment(
+    sender_email: str,
+    gmail_message_id: str,
+    attachment_id: str,
+    filename: str | None = None,
+):
     try:
         service = get_gmail_service(sender_email)
         attachment = (
