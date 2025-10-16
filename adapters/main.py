5bafb11ba6e16116f2487131b1726c224dc2b304
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-10-16 15:26:44 +0530

    updated the assistant wakeup webhook to rely on the assistant id rather than the assistant phoen number, now that the phone number doesn't need to exist for creating hte assistant

--- adapters/main.py
+++ adapters/main.py
@@ -358,11 +358,11 @@ def log_pre_hire_chats_webhook(request: Request):
 @functions_framework.http
 def assistant_wakeup_webhook(request: Request):
     print("assistant_wakeup_webhook function started")
-    assistant_number = request.form.get("assistant_number")
-    print(f"Assistant {assistant_number} woke up")
+    assistant_id = request.form.get("assistant_id")
+    print(f"Assistant {assistant_id} woke up")
 
     # get assistant data
-    assistant_data = get_assistant(phone_number=assistant_number)
+    assistant_data = get_assistant(phone_number=assistant_id)
     api_key = assistant_data["api_key"]
     assistant_id = assistant_data["assistant_id"]
     user_id = assistant_data["user_id"]
