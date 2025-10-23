546a2baa7fb09c5053d2bef5f33a9d1e32ce7312
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-10-23 17:01:19 +0530

    reverted the changes to the specs to test if the timeout change along with the prewarm stuff in unity makes a difference

--- communication/infra/helpers.py
+++ communication/infra/helpers.py
@@ -233,8 +233,8 @@ def create_unity_job(
                                 ],
                                 "env": env_vars,
                                 "resources": {
-                                    "requests": {"cpu": "4", "memory": "16Gi"},
-                                    "limits": {"cpu": "4", "memory": "16Gi"},
+                                    "requests": {"cpu": "2", "memory": "8Gi"},
+                                    "limits": {"cpu": "2", "memory": "8Gi"},
                                 },
                                 "volumeMounts": [
                                     {
