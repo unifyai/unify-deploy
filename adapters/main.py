a65dc0e8daa057ecc1897cf286475d117f3104bc
vedpatwardhan <vedpatwardhan@users.noreply.github.com>
2025-12-03 12:21:53 +0530

    updated idle check logic the final time

--- adapters/main.py
+++ adapters/main.py
@@ -814,7 +814,7 @@ def idle_job_cleaner(request):
 
         # check if job is idle
         if (
-            "Received EVENT: Ping(" in logs
+            "ping received - keeping conversation manager alive" in logs
             and "Inactivity timeout reached (360s), requesting shutdown..." not in logs
             and "Graceful shutdown completed" not in logs
             and "Shutting down convo manager..." not in logs
