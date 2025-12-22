ba9b64560dd3c984fc60e4beca92687baeb764fc
juliagsy <67888047+juliagsy@users.noreply.github.com>
2025-12-22 15:44:04 +0800

    renamed JOB_NAME to UNITY_CONVERSATION_JOB_NAME after updates in unity so that job_name is not default empty string

--- communication/infra/helpers.py
+++ communication/infra/helpers.py
@@ -150,7 +150,7 @@ def create_unity_job(
     try:
         # Define the assistant-specific environment variables
         env_vars = [
-            {"name": "JOB_NAME", "value": job_name},
+            {"name": "UNITY_CONVERSATION_JOB_NAME", "value": job_name},
             {
                 "name": "GOOGLE_APPLICATION_CREDENTIALS",
                 "value": "/secrets/key.json",
